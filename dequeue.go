f0342c697e9a986a306aa5ca2f777c5d3b4d1c31
Charles <charles@reptile.ca>
2013-05-15 14:21:31 -0700

    dev work. still work in progress.

--- dequeue.go
+++ dequeue.go
@@ -4,7 +4,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/streadway/amqp"
+//	"github.com/streadway/amqp"
+	"labix.org/v2/mgo"
 	"log/syslog"
 	"math/rand"
 	"time"
@@ -12,10 +13,50 @@ import (
 
 
 
-func dequeue(syslogLog *syslog.Writer) {
+func dequeue(syslogLog *syslog.Writer, mongoHref string, mongoDatabaseName string, mongoCollectionName string) {
 
 	//DEBUG
 	syslogLog.Notice("[dequeue] BEGIN")
+	syslogLog.Notice(  fmt.Sprintf("    [dequeue] mongoHref = [%v]", mongoHref)  )
+
+	// Deal with parameters.
+		if "" == mongoHref {
+			errMsg := fmt.Sprintf("    [dequeue] Bad mongoHref. Received: [%v].", mongoHref)
+			syslogLog.Err(errMsg)
+			panic(errMsg)
+/////////////////////// RETURN
+			return
+		}
+
+		if "" == mongoDatabaseName {
+			errMsg := fmt.Sprintf("    [dequeue] Bad mongoDatabaseName. Received: [%v].", mongoDatabaseName)
+			syslogLog.Err(errMsg)
+			panic(errMsg)
+/////////////////////// RETURN
+			return
+		}
+
+		if "" == mongoCollectionName {
+			errMsg := fmt.Sprintf("    [dequeue] Bad mongoCollectionName. Received: [%v].", mongoCollectionName)
+			syslogLog.Err(errMsg)
+			panic(errMsg)
+/////////////////////// RETURN
+			return
+		}
+
+
+	// Connect to MongoDB
+		session, err := mgo.Dial(mongoHref)
+		if err != nil {
+			syslogLog.Err( fmt.Sprintf("    [dequeue] Could NOT connect to MongoDB at [%v], received err = [%v]", mongoHref, err) )
+			panic(err)
+/////////////////////// RETURN
+			return
+		}
+		defer session.Close()
+
+		// Optional. Switch the session to a monotonic behavior.
+		session.SetMode(mgo.Monotonic, true)
 
 
 	// Forever
@@ -27,12 +68,18 @@ func dequeue(syslogLog *syslog.Writer) {
 
 
 
-			sleepTime := time.Duration(5 + rand.Intn(7)) * time.Second
 
-			//DEBUG
-			syslogLog.Notice( fmt.Sprintf("    [dequeue] sleep for %v", sleepTime) )
+			// Check MongoDB for items that are ready.
 
-			time.Sleep(sleepTime)
+
+
+			// Sleep for a while before checking again.
+				sleepTime := time.Duration(5 + rand.Intn(7)) * time.Second
+
+				//DEBUG
+				syslogLog.Notice( fmt.Sprintf("    [dequeue] sleep for %v", sleepTime) )
+
+				time.Sleep(sleepTime)
 		} // for
 
 
