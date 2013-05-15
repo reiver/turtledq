0acc9b7addecb22d6918d86f1e253374cbdc430a
Charles <charles@reptile.ca>
2013-05-15 11:16:16 -0700

    commented out ZeroMQ stuff for now

--- turtledq.go
+++ turtledq.go
@@ -5,7 +5,7 @@ package main
 import (
 	"fmt"
 	"github.com/msbranco/goconfig"
-	"github.com/vaughan0/go-zmq"
+//	"github.com/vaughan0/go-zmq"
 	"log"
 	"log/syslog"
 	"os"
@@ -76,46 +76,48 @@ func main() {
 		go enqueue(syslogLog)
 
 
-	// Create ZeroMQ context.
-		ctx, err := zmq.NewContext()
-		if err != nil {
-			panic(err)
-		}
-		defer ctx.Close()
+//	// Create ZeroMQ context.
+//		ctx, err := zmq.NewContext()
+//		if err != nil {
+//			panic(err)
+//		}
+//		defer ctx.Close()
+//
+//
+//	// Create ZeroMQ socket (from ZeroMQ context).
+//		sock, err := ctx.Socket(zmq.Router)
+//		if err != nil {
+//			panic(err)
+//		}
+//		defer sock.Close()
+//
+//		if err = sock.Bind("tcp://*:5555"); err != nil {
+//			panic(err)
+//		}
+//
+//
+//	// Handle input from ZeroMQ (via channels).
+//		chans := sock.Channels()
+//		defer chans.Close()
+//
+//
+//
+//		for {
+//			select {
+//
+//				case msg := <-chans.In():
+//					go func() {
+//						resp := handleRequest(syslogLog, msg)
+//						chans.Out() <- resp
+//					}()
+//
+//				case err := <-chans.Errors():
+//					panic(err)
+//
+//			} // select
+//		}
 
-
-	// Create ZeroMQ socket (from ZeroMQ context).
-		sock, err := ctx.Socket(zmq.Router)
-		if err != nil {
-			panic(err)
-		}
-		defer sock.Close()
-
-		if err = sock.Bind("tcp://*:5555"); err != nil {
-			panic(err)
-		}
-
-
-	// Handle input from ZeroMQ (via channels).
-		chans := sock.Channels()
-		defer chans.Close()
-
-
-
-		for {
-			select {
-
-				case msg := <-chans.In():
-					go func() {
-						resp := handleRequest(syslogLog, msg)
-						chans.Out() <- resp
-					}()
-
-				case err := <-chans.Errors():
-					panic(err)
-
-			} // select
-		}
+		select{}
 
 
 	//DEBUG
