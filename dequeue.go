ff053174d4ac1571c437d383f8d87e1b21414ecc
Charles <charles@reptile.ca>
2013-05-14 23:32:01 -0700

    mode dequeue stuff out of main file

---
+++ dequeue.go
@@ -0,0 +1,41 @@
+package main
+
+
+
+import (
+	"fmt"
+	"log/syslog"
+	"math/rand"
+	"time"
+)
+
+
+
+func dequeue(syslogLog *syslog.Writer) {
+
+	//DEBUG
+	syslogLog.Notice("[dequeue] BEGIN")
+
+
+	// Forever
+		for {
+
+			//DEBUG
+			syslogLog.Notice("    [dequeue] =-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=")
+			syslogLog.Notice("    [dequeue] loop")
+
+
+
+			sleepTime := time.Duration(5 + rand.Intn(7)) * time.Second
+
+			//DEBUG
+			syslogLog.Notice( fmt.Sprintf("    [dequeue] sleep for %v", sleepTime) )
+
+			time.Sleep(sleepTime)
+		} // for
+
+
+	//DEBUG
+	syslogLog.Notice("[dequeue] END")
+
+}
--- turtledq.go
+++ turtledq.go
@@ -7,8 +7,6 @@ import (
 	"github.com/vaughan0/go-zmq"
 	"log"
 	"log/syslog"
-	"math/rand"
-	"time"
 )
 
 
@@ -91,34 +89,4 @@ func handleRequest(syslogLog *syslog.Writer, msg [][]byte) [][]byte {
 		return [][]byte{
 			[]byte("apple banana cherry"),
 		}
-}
-
-
-func dequeue(syslogLog *syslog.Writer) {
-
-	//DEBUG
-	syslogLog.Notice("[dequeue] BEGIN")
-
-
-	// Forever
-		for {
-
-			//DEBUG
-			syslogLog.Notice("    [dequeue] =-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=")
-			syslogLog.Notice("    [dequeue] loop")
-
-
-
-			sleepTime := time.Duration(5 + rand.Intn(7)) * time.Second
-
-			//DEBUG
-			syslogLog.Notice( fmt.Sprintf("    [dequeue] sleep for %v", sleepTime) )
-
-			time.Sleep(sleepTime)
-		} // for
-
-
-	//DEBUG
-	syslogLog.Notice("[dequeue] END")
-
 }
