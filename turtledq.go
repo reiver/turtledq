d783a17dcc0a8fc386910077c6f926b1168a35c7
Charles <charles@reptile.ca>
2013-05-15 07:25:43 -0700

    added support for using config file

--- turtledq.go
+++ turtledq.go
@@ -4,9 +4,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/msbranco/goconfig"
 	"github.com/vaughan0/go-zmq"
 	"log"
 	"log/syslog"
+	"os"
+	"strings"
 )
 
 
@@ -25,6 +28,47 @@ func main() {
 	//DEBUG
 	syslogLog.Notice("=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=-<>-=")
 	syslogLog.Notice("TurtleDQ Server BEGIN.")
+
+
+	// Config
+		//DEBUG
+		syslogLog.Notice("    Settings BEGIN.")
+
+		cmdPath := os.Args[0]
+		cmdPathLastSlash := strings.LastIndexFunc(cmdPath, func(c rune) bool {
+			return '/' == c
+		})
+
+		cmdDirPath := cmdPath[0:cmdPathLastSlash]
+
+		//DEBUG
+		syslogLog.Notice( fmt.Sprintf("    command path: [%v]", cmdPath)  )
+		syslogLog.Notice( fmt.Sprintf("    command path last slash: [%v]", cmdPathLastSlash) )
+		syslogLog.Notice( fmt.Sprintf("    command dir path: [%v]", cmdDirPath) )
+
+		confRelativePath := "turtledq.ini"
+		confPath := cmdDirPath + "/" + confRelativePath
+
+		//DEBUG
+		syslogLog.Notice( fmt.Sprintf("    settings file relative path: [%v]", confRelativePath) )
+		syslogLog.Notice( fmt.Sprintf("    settings file absolute path: [%v]", confPath) )
+
+		//c, err := configfile.ReadConfigFile(confPath);
+		c, err := goconfig.ReadConfigFile(confPath);
+		if nil != err {
+			errMsg := fmt.Sprintf("Error when trying to read config file: err = [%v]", err)
+			syslogLog.Err(errMsg)
+			log.Fatal(errMsg)
+/////////// RETURN
+			return
+		}
+
+		//DEBUG
+		syslogLog.Notice( fmt.Sprintf("config file: [%v]", c) )
+
+		//DEBUG
+		syslogLog.Notice("    Settings END.")
+
 
 
 	// Go!
