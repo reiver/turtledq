601ba351bd0824dd9f81d42716833405a7aa92ba
Charles <charles@reptile.ca>
2013-05-15 16:37:53 -0700

    some work on config stuff

--- turtledq.go
+++ turtledq.go
@@ -4,12 +4,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/msbranco/goconfig"
 //	"github.com/vaughan0/go-zmq"
 	"log"
 	"log/syslog"
-	"os"
-	"strings"
 )
 
 
@@ -31,48 +28,20 @@ func main() {
 
 
 	// Config
-		//DEBUG
-		syslogLog.Notice("    Settings BEGIN.")
-
-		cmdPath := os.Args[0]
-		cmdPathLastSlash := strings.LastIndexFunc(cmdPath, func(c rune) bool {
-			return '/' == c
-		})
-
-		cmdDirPath := cmdPath[0:cmdPathLastSlash]
-
-		//DEBUG
-		syslogLog.Notice( fmt.Sprintf("    command path: [%v]", cmdPath)  )
-		syslogLog.Notice( fmt.Sprintf("    command path last slash: [%v]", cmdPathLastSlash) )
-		syslogLog.Notice( fmt.Sprintf("    command dir path: [%v]", cmdDirPath) )
-
-		confRelativePath := "turtledq.ini"
-		confPath := cmdDirPath + "/" + confRelativePath
-
-		//DEBUG
-		syslogLog.Notice( fmt.Sprintf("    settings file relative path: [%v]", confRelativePath) )
-		syslogLog.Notice( fmt.Sprintf("    settings file absolute path: [%v]", confPath) )
-
-		//c, err := configfile.ReadConfigFile(confPath);
-		c, err := goconfig.ReadConfigFile(confPath);
+		turtleConfig, err := NewTurtleConfig(syslogLog)
 		if nil != err {
-			errMsg := fmt.Sprintf("Error when trying to read config file: err = [%v]", err)
+			errMsg := fmt.Sprintf("Error with config file: err = [%v]", err)
+
 			syslogLog.Err(errMsg)
 			log.Fatal(errMsg)
-/////////// RETURN
+/////////////////////// RETURN
 			return
 		}
-
-		//DEBUG
-		syslogLog.Notice( fmt.Sprintf("config file: [%v]", c) )
-
-		//DEBUG
-		syslogLog.Notice("    Settings END.")
 
 
 
 	// Go!
-		go dequeue(syslogLog)
+		go dequeue(syslogLog, turtleConfig.DequeueMongoHref, turtleConfig.DequeueMongoDatabaseName, turtleConfig.DequeueMongoCollectionName)
 		go enqueue(syslogLog)
 
 
