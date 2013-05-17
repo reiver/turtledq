8dc20c88fac03a63a36b40d4348db9ce18b69162
Charles <charles@reptile.ca>
2013-05-17 12:17:04 -0700

    dev

--- enqueue.go
+++ enqueue.go
@@ -4,7 +4,7 @@ package main
 
 import (
 	"fmt"
-//	"github.com/streadway/amqp"
+	"github.com/streadway/amqp"
 	"log/syslog"
 	"math/rand"
 	"time"
@@ -53,6 +53,50 @@ func enqueue(syslogLog *syslog.Writer, amqpHref string, amqpExchange string, amq
 /////////////////////// RETURN
 			return
 		}
+
+
+	// Connect to AMQP.
+		amqpConnection, err := amqp.Dial(amqpHref)
+		if err != nil {
+			syslogLog.Err(  fmt.Sprintf("    [enqueue] Could NOT connect to AMQP server (RabbitMQ?) at [%v], received err = [%v]", amqpHref, err)  )
+			panic(err)
+/////////////////////// RETURN
+			return
+		}
+		defer amqpConnection.Close()
+
+		amqpChannel, err := amqpConnection.Channel()
+		if err != nil {
+			syslogLog.Err("    [enqueue] Could NOT get AMQP channel")
+			panic(err)
+/////////////////////// RETURN
+			return
+		}
+
+		//DEBUG
+		syslogLog.Notice(  fmt.Sprintf("    [enqueue] amqpChannel = [%v]", amqpChannel)  )
+
+
+
+	// AMQP queue bind.
+		amqpKey := amqpQueue
+
+		if err = amqpChannel.QueueBind(
+			amqpQueue,    // name of the queue
+			amqpKey,      // bindingKey
+			amqpExchange, // sourceExchange
+			false,        // noWait
+			nil,          // arguments
+		); err != nil {
+			syslogLog.Err(  fmt.Sprintf("    [enqueue] Could NOT bind to queue [%v] on AMQP server (RabbitMQ?) at [%v], received err = [%v]", amqpQueue, amqpHref, err)  )
+			panic(err)
+/////////////////////// RETURN
+			return
+		}
+
+		//DEBUG
+		syslogLog.Notice("    [enqueue] Queue bound.")
+
 
 
 
